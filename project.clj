74a3aefc6af833848471fe5e4681dc02aadec814
mbme <stribog.ua@gmail.com>
2015-01-27 22:58:46 +0200

    profile cleanup

--- project.clj
+++ project.clj
@@ -1,31 +1,45 @@
-(defproject vita "0.1.0-SNAPSHOT"
-  :description "Vita UI"
+(def clojure     "1.6.0")
+
+(def http-kit    "2.1.19")
+(def compojure   "1.3.1")
+(def ring        "1.3.2")
+(def cheshire    "5.4.0")
+(def timbre      "3.3.1")
+
+(def servlet-api "2.5")
+(def ancient     "0.6.1")
+(def cider       "0.8.2")
+
+
+(defproject skelet "0.1.0-SNAPSHOT"
+  :description "Skelet - backend for Vita"
+
   :global-vars {*warn-on-reflection* true}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :main vita.handler
+
+  :dependencies [[org.clojure/clojure ~clojure]
 
                  ;; server, routes
-                 [http-kit "2.1.19"]
-                 [compojure "1.3.1"
+                 [http-kit ~http-kit]
+                 [compojure ~compojure
                   :exclusions [commons-codec]]
-                 [ring/ring-core "1.3.2"
+                 [ring/ring-core ~ring
                   :exclusions [org.clojure/tools.reader]]
 
                  ;; serialization/deserialization
-                 [cheshire "5.4.0"]
+                 [cheshire ~cheshire]
 
                  ;; logging
-                 [com.taoensso/timbre "3.3.1"
-                  :exclusions [org.clojure/tools.reader]]]
+                 [com.taoensso/timbre ~timbre]]
 
   :exclusions [org.clojure/clojure]
 
-  :plugins [[lein-ancient   "0.6.1" :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-ancient ~ancient :exclusions [org.clojure/clojure]]]
 
   :profiles {:dev
-             {:dependencies [[ring/ring-devel "1.3.2"]
-                             [cider/cider-nrepl "0.8.2"
-                              :exclusions [org.clojure/java.classpath]]
-                             [javax.servlet/servlet-api "2.5"]]}}
-
-  :main vita.handler)
+             {:dependencies
+              [[ring/ring-devel ~ring]
+               [cider/cider-nrepl ~cider
+                :exclusions [org.clojure/java.classpath]]
+               [javax.servlet/servlet-api ~servlet-api]]}})
